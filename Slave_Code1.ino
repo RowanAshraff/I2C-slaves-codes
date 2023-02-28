b2cf0569afa55fdc22355e8e42934e54917ea47e
Rowan <88684837+RowanAshraff@users.noreply.github.com>
2023-02-28 22:37:37 +0200

    Update Slave_Code1.ino

--- Slave_Code1.ino
+++ Slave_Code1.ino
@@ -1,9 +1,10 @@
 #include <Wire.h>                            
 #define SlaveAdress 1  // Adress of this slave
 
-int outVal[] = {0, 0, 0, 0, 0, 0, 0, 0};   // An array for the values of the PWM pins 
-int outPin[] = {2, 3, 4, 5, 6, 7, 8, 9};   // An array for the PWM pins of the microcontroller
-int counter = 0, counterAnalog = 0, val = 0;
+int outVal[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};   // An array for the values of the PWM pins 
+int outPin[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};   // An array for the PWM/digital pins of the microcontroller
+                                                           //Pins 10-13 are Digital+ pins3
+int counter = 0, val = 0;
   
 
 int analogVal[] = {200, 150, 100, 90, 80, 70, 50, 10};    // An array for the values of the analog pins
@@ -16,7 +17,6 @@ void setup()
     pinMode(outPin[i], OUTPUT);
     pinMode(analogPin[i], INPUT);         
   }
-  
   Wire.begin(SlaveAdress);                                
   Wire.onReceive(RecieveEvent); 
   Wire.onRequest(requestEvent);  
@@ -27,33 +27,35 @@ void setup()
 
 void loop() 
 {
-  delay(100);
+  for(int k = 0; k < 8; k++){
+    analogVal[k] = analogRead(analogPin[k]);
+    analogVal[k] = mapping(analogVal[k], 0,1023,0,255);     // Converting it to the specified range
+    }
 }
+
 
 void RecieveEvent(int Press)    // A function that runs each time a master sends a number
 {
-  while(Wire.available()){ 
-    outVal[counter] =  Wire.read();
-//    Serial.println(outVal[counter]);                   
-    counter += 1;
-    if (counter == 8){    // Get 8 PWM readings from the master then resetting it
-      counter = 0;   
+  while(Wire.available()){
+    if(Wire.read() == 's'){
+      Serial.println("recieving: ");
+      for(int i = 0; i < 12; i++){
+        outVal[i] =  Wire.read();
+        Serial.println(String(outVal[i]));
+      }
     }
   }
 }
 
 
 void requestEvent(){ // A function that runs each time a master requests a byte
-  analogVal[counterAnalog] = analogRead(analogPin[counterAnalog]);                // Reading analog pins values
-  analogVal[counterAnalog] = mapping(analogVal[counterAnalog], 0,1023,0,255);     // Converting it to the specified range
-
-  Wire.write(analogVal[counterAnalog]);              // Sending the value to the master
-  counterAnalog += 1;
-  if (counterAnalog==8){ //Resetting the counter once 8 pins have been read and their values sent
-    counterAnalog = 0;
+  Serial.println(String(analogVal[0]));
+  Wire.write(analogVal[0]);              // Sending the value to the master
+  counter += 1;
+  if (counter == 8){ //Resetting the counter once 8 pins have been read and their values sent
+    counter = 0;
   }
 }
-
 
 int mapping(int value, int startPoint1, int endPoint1, int startPoint2, int endPoint2){  // It acts exactly like the arduino "map" function
   return(value*(float(endPoint2-startPoint2)/float(endPoint1-startPoint1)));
